19ec407f0aba10903eacdd4415db0b33c3e6df98
flizcano <flizcanor@icloud.com>
2023-02-12 20:54:32 -0500

    se realizan ajustes finales

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -2,7 +2,7 @@
 .curso-main-container.pb-3
   BannerInterno(icono="fas fa-tasks" titulo="Actividad didáctica")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-    
+
     .row.mb-5.justify-content-center.align-items-center.align-items-lg-stretch
       .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
         .tarjeta.h-100.d-flex.align-items-center.p-4
@@ -10,17 +10,17 @@
             img(src="@/assets/template/arrastrar.svg", alt="Texto que describa la imagen")
       .col-12.col-md-8.col-lg-9
         .titulo-segundo
-          h2 Arrastrar y soltar
+          h2 Seguridad y salud en el trabajo
         p.mb-4 A continuación, encontrará conceptos y definiciones abordadas a lo largo del componente formativo, con el objetivo de evaluar la aprehensión de estos. Arrastre cada concepto a la definición correspondiente.
         .tarjeta.actividad.p-3
-          .row.justify-content-around.align-items-center            
+          .row.justify-content-around.align-items-center
             .col-sm.mb-3.mb-sm-0
               p.fw-bold.mb-0 Arrastrar y soltar
             .col-auto
               a.boton.boton--b(:href="obtenerLink('/actividades/actividad.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
-                
+
 </template>
 
 <script>
